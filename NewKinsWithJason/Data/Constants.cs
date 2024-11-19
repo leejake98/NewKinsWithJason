8403813c843b945be01f8a6e7ac535d0edd1bf28
leejake98 <leejake98@gmail.com>
2024-11-19 13:02:34 +0900

    초기 커밋.

---
+++ NewKinsWithJason/Data/Constants.cs
@@ -0,0 +1,23 @@
+﻿namespace NewKinsWithJason.Data
+{
+    public class Constants
+    {
+        // Database filename
+        public const string DatabaseFilename = "workflow.json";
+
+
+
+        // Database path
+        public static string DatabasePath
+        {
+            get
+            {
+                // Set the base path to the project root folder
+                var basePath = AppContext.BaseDirectory;
+
+                // Return the full path to the database file
+                return Path.Combine(basePath, "wwwroot", DatabaseFilename);
+            }
+        }
+    }
+}
